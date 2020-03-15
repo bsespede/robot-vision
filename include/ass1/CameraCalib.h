1e552a35af1ada482f3c1373b81b623de1c2dca4
Braulio Sespede <bsespede@itba.edu.ar>
2020-03-15 10:18:41 +0100

    Task1 in progress

--- include/ass1/CameraCalib.h
+++ include/ass1/CameraCalib.h
@@ -5,8 +5,21 @@
 #ifndef ROBOT_VISION_SRC_ASS1_CAMERACALIB_H_
 #define ROBOT_VISION_SRC_ASS1_CAMERACALIB_H_
 
-class CameraCalib {
+#include <opencv2/core.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
+class CameraCalib {
+ public:
+  CameraCalib(cv::Size boardSize, float squareLength);
+  void computeIntrinsics(std::string inputPath, std::string outputPath, bool printResults);
+ private:
+  void printDetections(std::string outputPath);
+  void detectCorners(cv::Mat boardImage);
+
+  cv::Size boardSize;
+  float squareLength;
+  std::vector<std::vector<cv::Point2f>> detections;
 };
 
 #endif //ROBOT_VISION_SRC_ASS1_CAMERACALIB_H_
--- src/ass1/CameraCalib.cpp
+++ src/ass1/CameraCalib.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "ass1/CameraCalib.h"
+
+CameraCalib::CameraCalib(cv::Size boardSize, int squareLength) {
+
+}
+
+void CameraCalib::computeIntrinsics(std::string inputPath, std::string outputPath, bool printResults) {
+
+}
+
+void CameraCalib::printDetections(std::string outputPath) {
+
+}
+
+void CameraCalib::detectCorners(cv::Mat boardImage) {
+
+}
